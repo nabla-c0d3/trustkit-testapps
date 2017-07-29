911c79675dbe5bf8fa1e82f907afce1f599b5590
Alban Diquet <ad@sourcetheorem.com>
2017-07-28 18:51:13 -0700

    Integrate TrustKit via Carthage

--- TestCarthage/TestCarthage/AppDelegate.m
+++ TestCarthage/TestCarthage/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import <TrustKit/TrustKit.h>
+
 
 @interface AppDelegate ()
 
@@ -17,6 +19,45 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    NSDictionary *trustKitConfig =
+    @{
+      kTSKSwizzleNetworkDelegates: @YES,
+      
+      kTSKPinnedDomains: @{
+              
+              // Pin invalid SPKI hashes to *.yahoo.com to demonstrate pinning failures
+              @"yahoo.com": @{
+                      kTSKEnforcePinning: @YES,
+                      kTSKIncludeSubdomains: @YES,
+                      kTSKPublicKeyAlgorithms: @[kTSKAlgorithmRsa2048],
+                      
+                      // Wrong SPKI hashes to demonstrate pinning failure
+                      kTSKPublicKeyHashes: @[
+                              @"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
+                              @"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB="
+                              ],
+                      
+                      // Send reports for pinning failures
+                      // Email info@datatheorem.com if you need a free dashboard to see your App's reports
+                      kTSKReportUris: @[@"https://overmind.datatheorem.com/trustkit/report"]
+                      },
+              
+              
+              // Pin valid SPKI hashes to www.datatheorem.com to demonstrate success
+              @"www.datatheorem.com" : @{
+                      kTSKEnforcePinning:@YES,
+                      kTSKPublicKeyAlgorithms : @[kTSKAlgorithmEcDsaSecp384r1],
+                      
+                      // Valid SPKI hashes to demonstrate success
+                      kTSKPublicKeyHashes : @[
+                              @"58qRu/uxh4gFezqAcERupSkRYBlBAvfcw7mEjGPLnNU=", // CA key: COMODO ECC Certification Authority
+                              @"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=", // Fake key but 2 pins need to be provided
+                              ]
+                      }}};
+    
+    [TrustKit initSharedInstanceWithConfiguration:trustKitConfig];
+    NSLog(@"TEST %@", TrustKit.sharedInstance.pinningValidator);
     return YES;
 }
 
